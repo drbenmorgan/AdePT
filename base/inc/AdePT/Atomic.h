adc80cb71724514c1b5fad80702bda97e9e2cab3
Ben Morgan <ben.morgan@warwick.ac.uk>
2020-10-27 16:22:03 +0000

    Attempt to migrate Atomic and test to "Allen-style"
    Default CopCore build to target CUDA for now, as CPU targetting requires
    more work in AdePT.
    
    Use CopCore header and macros to annotate AdePT Atomic member functions
    for host/device and target device. Update test_atomic program to unify
    compilation.
    
    NB: This is known to fail compilation even in CUDA target with errors:
    
    .../AdePT.git/base/inc/AdePT/Atomic.h(33):
    error: calling a __device__ function("load") from a __host__ __device__
    function("Atomic_t") is not allowed
    
    .../AdePT.git/base/inc/AdePT/Atomic.h(37):
    error: calling a __device__ function("load") from a __host__ __device__
    function("operator=") is not allowed
    
    The source of these errors is not clear as yet. The VecCore macros
    *should* expand to the same as the CopCore/Allen ones when targetting
    CUDA so why compilation works with VecCore and not CopCore.
    
    It was noted that the device-only functions in Atomic should be
    host-device, but they should also be device-only when built with
    VecCore.

--- base/inc/AdePT/Atomic.h
+++ base/inc/AdePT/Atomic.h
@@ -10,10 +10,11 @@
 #ifndef ADEPT_ATOMIC_H_
 #define ADEPT_ATOMIC_H_
 
-#include <VecCore/VecCore>
+#include <CopCore/CopCore.h>
 #include <cassert>
 
-#ifndef VECCORE_CUDA_DEVICE_COMPILATION
+//#ifndef VECCORE_CUDA_DEVICE_COMPILATION
+#ifdef TARGET_DEVICE_CPU
 #include <atomic>
 #endif
 
@@ -24,19 +25,19 @@ namespace adept {
 template <typename Type>
 struct Atomic_t {
   /** @brief Constructor taking an address */
-  VECCORE_ATT_HOST_DEVICE
+  __host__ __device__
   Atomic_t() : fData{0} {}
 
   /** @brief Copy constructor */
-  VECCORE_ATT_HOST_DEVICE
+  __host__ __device__
   Atomic_t(Atomic_t const &other) { store(other.load()); }
 
   /** @brief Assignment */
-  VECCORE_ATT_HOST_DEVICE
+  __host__ __device__
   Atomic_t &operator=(Atomic_t const &other) { store(other.load()); }
 
   /** @brief Emplace the data at a given address */
-  VECCORE_ATT_HOST_DEVICE
+  __host__ __device__
   static Atomic_t *MakeInstanceAt(void *addr)
   {
     assert(addr != nullptr && "cannot allocate at nullptr address");
@@ -45,7 +46,8 @@ struct Atomic_t {
     return obj;
   }
 
-#ifndef VECCORE_CUDA_DEVICE_COMPILATION
+// #ifndef VECCORE_CUDA_DEVICE_COMPILATION
+#ifdef TARGET_DEVICE_CPU
   /// Standard library atomic behaviour.
   using AtomicType_t = std::atomic<Type>;
 
@@ -100,7 +102,7 @@ struct Atomic_t {
   using AtomicType_t = Type;
 
   /** @brief Atomically assigns the desired value to the atomic variable. */
-  VECCORE_ATT_DEVICE
+  __device__
   Type operator=(Type desired)
   {
     atomicExch(&fData, desired);
@@ -108,40 +110,40 @@ struct Atomic_t {
   }
 
   /** @brief Atomically replaces the current value with desired. */
-  VECCORE_ATT_DEVICE
+  __device__
   void store(Type desired) { atomicExch(&fData, desired); }
 
   /** @brief Atomically loads and returns the current value of the atomic variable. */
-  VECCORE_ATT_DEVICE
+  __device__
   Type load() const { return fData; }
 
   /** @brief Atomically replaces the underlying value with desired. */
-  VECCORE_ATT_DEVICE
+  __device__
   Type exchange(Type desired) { return atomicExch(&fData, desired); }
 
   /** @brief Atomically replaces the current value with the result of arithmetic addition of the value and arg. */
-  VECCORE_ATT_DEVICE
+  __device__
   Type fetch_add(Type arg) { return atomicAdd(&fData, arg); }
 
   /** @brief Atomically replaces the current value with the result of arithmetic subtraction of the value and arg. */
-  VECCORE_ATT_DEVICE
+  __device__
   Type fetch_sub(Type arg) { return atomicAdd(&fData, -arg); }
 
   /** @brief Atomically replaces the current value with the result of bitwise AND of the value and arg. */
-  VECCORE_ATT_DEVICE
+  __device__
   Type fetch_and(Type arg) { return atomicAnd(&fData, arg); }
 
   /** @brief Atomically replaces the current value with the result of bitwise OR of the value and arg. */
-  VECCORE_ATT_DEVICE
+  __device__
   Type fetch_or(Type arg) { return atomicOr(&fData, arg); }
 
   /** @brief Atomically replaces the current value with the result of bitwise XOR of the value and arg. */
-  VECCORE_ATT_DEVICE
+  __device__
   Type fetch_xor(Type arg) { return atomicXor(&fData, arg); }
 
   /** @brief Atomically compares the stored value with the expected one, and if equal stores desired.
    * If not loads the old value into expected. Returns true if swap was successful. */
-  VECCORE_ATT_DEVICE
+  __device__
   bool compare_exchange_strong(Type &expected, Type desired)
   {
     Type old    = atomicCAS(&fData, expected, desired);
@@ -151,19 +153,19 @@ struct Atomic_t {
   }
 
   /** @brief Performs atomic pre-increment. */
-  VECCORE_ATT_DEVICE
+  __device__
   Type operator++() { return (fetch_add(1) + 1); }
 
   /** @brief Performs atomic post-increment. */
-  VECCORE_ATT_DEVICE
+  __device__
   Type operator++(int) { return fetch_add(1); }
 
   /** @brief Performs atomic pre-decrement. */
-  VECCORE_ATT_DEVICE
+  __device__
   Type operator--() { return (fetch_sub(1) - 1); }
 
   /** @brief Performs atomic post-decrement. */
-  VECCORE_ATT_DEVICE
+  __device__
   Type operator--(int) { return fetch_sub(1); }
 #endif
 
